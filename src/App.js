ba9d0b0ef39792b1360522c3c4b90b7a4699a035
tanlucdev <tanluc.dev@gmail.com>
2024-05-10 17:04:16 +0700

    Create auth page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css"
 import Home from "./page/home/Home"
 import Profile from "./page/Profile/Profile";
+import Auth from "./page/Auth/Auth";
 function App() {
   return (
     <div className="App">
       <div className="blur" style={{ top: '-18%', right: '0' }}></div>
       <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
       {/* <Home /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <Auth />
     </div>
 
   );
---
+++ src/page/Auth/Auth.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import './Auth.scss'
+import Logo from '../../img/logo.png'
+const Auth = () => {
+  return (
+    <div className="Auth">
+      <div className="auth-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>ZKC Media</h1>
+          <h6>Explore the ideas throughout the world</h6>
+        </div>
+      </div>
+      <LogIn />
+    </div>
+  )
+}
+
+function LogIn() {
+  return (
+    <div className="a-right">
+      <form className="infoForm authForm">
+        <h3>Log In</h3>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            className="infoInput"
+            name="username"
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            className="infoInput"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+
+        <div>
+          <span style={{ fontSize: "12px" }}>
+            Don't have an account Sign up
+          </span>
+          <button className="button infoButton">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function SignUp() {
+  return (
+    <div className='auth-right'>
+      <form className="infoForm authForm">
+        <h3>Sign Up</h3>
+        <div>
+          <input
+            type="text"
+            placeholder='First Name'
+            className='infoInput'
+            name='firstname'
+          />
+          <input
+            type="text"
+            placeholder='Last Name'
+            className='infoInput'
+            name='lastname'
+          />
+        </div>
+        <div>
+          <input type="text"
+            placeholder='Usernames'
+            className='infoInput'
+            name='username'
+          />
+        </div>
+        <div>
+          <input type="text"
+            placeholder='Password'
+            className='infoInput'
+            name='password'
+          />
+          <input type="text"
+            placeholder='Confirm Password'
+            className='infoInput'
+            name='confirmpass'
+          />
+        </div>
+        <div>
+          <span style={{ fontSize: '12px' }}> Already have an account. Login!</span>
+        </div>
+
+        <button className='button infoButton' type='submit'>Signup</button>
+      </form>
+    </div>
+  )
+}
+
+export default Auth
